1ae1a42462448bc20b4d6ca74a6569de1ea58a94
Robert Wilson <rjw11@sfu.ca>
2023-10-21 11:24:39 -0700

    added movement to player

--- main.py
+++ main.py
@@ -2,5 +2,9 @@ import pygame
 from pygame.locals import *
  
 pygame.init()
-vec = pygame.math.Vector2  # 2 for two dimensional
+HEIGHT = 450
+WIDTH = 400
+ACC = 0.5
+FRIC = -0.12
+FPS = 60
 
--- player.py
+++ player.py
@@ -1,7 +1,7 @@
 import pygame
+from pygame.locals import *
 import main
 
-vec = pygame.math.Vector2  # 2 for two dimensional
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -10,6 +10,25 @@ class Player(pygame.sprite.Sprite):
         self.surf.fill((128,255,40))
         self.rect = self.surf.get_rect()
    
-        self.pos = vec((10, 385))
-        self.vel = vec(0,0)
-        self.acc = vec(0,0)
+        self.pos = main.vec((10, 385))
+        self.vel = main.vec(0,0)
+        self.acc = main.vec(0,0)
+
+    def move(self):
+        self.acc = main.vec(0,0) 
+        pressed_keys = pygame.key.get_pressed()            
+        if pressed_keys[K_LEFT]:
+            self.acc.x = -(main.ACC)
+        if pressed_keys[K_RIGHT]:
+            self.acc.x = main.ACC  
+
+        self.acc.x += self.vel.x * main.FRIC
+        self.vel += self.acc
+        self.pos += self.vel + 0.5 * self.acc
+
+        if self.pos.x > main.WIDTH:
+            self.pos.x = 0
+        if self.pos.x < 0:
+            self.pos.x = main.WIDTH
+     
+        self.rect.midbottom = self.pos
