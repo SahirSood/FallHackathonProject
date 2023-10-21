e563d6ca240f079dfd94cb97523d3dfc72b16742
Robert Wilson <rjw11@sfu.ca>
2023-10-21 12:14:39 -0700

    added player movement on screen

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import pygame
+from pygame.locals import *
+import player
 pygame.init()
 
 # Setting screen size
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.5)
+vec = pygame.math.Vector2
+FPS = 60
 
 # Creating a screen with a grey background
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -12,19 +16,31 @@ screen.fill((192, 192, 192))  # Color should be a tuple
 # Initializing color
 colour = (0, 128, 0)
 
+FramePerSec = pygame.time.Clock()
+
 # Coordinates for player
 x = 100
 y = 50
+p1 = player.Player()
 
 # Drawing a rectangle
 pygame.draw.rect(screen, colour, pygame.Rect(30, 30, 60, 60))
+
+
 
 run = True
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: 
             run = False
-
+    p1.move()
+    pygame.draw.rect(screen, colour, pygame.Rect(p1.pos[0], p1.pos[1], 5, 10))
+    
     pygame.display.update()
+    FramePerSec.tick(FPS)
+    screen.fill((0, 0, 0)) #clear screen
 
 pygame.quit()
+
+
+
--- player.py
+++ player.py
@@ -1,34 +1,40 @@
 import pygame
 from pygame.locals import *
-import main
 
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.5)
+vec = pygame.math.Vector2
+ACC = 0.5
+FRIC = -0.12
+FPS = 60
 
 class Player(pygame.sprite.Sprite):
+    
     def __init__(self):
         super().__init__()
         self.surf = pygame.Surface((30, 30))
         self.surf.fill((128,255,40))
         self.rect = self.surf.get_rect()
    
-        self.pos = main.vec((10, 385))
-        self.vel = main.vec(0,0)
-        self.acc = main.vec(0,0)
+        self.pos = vec((10, 385))
+        self.vel = vec(0,0)
+        self.acc = vec(0,0)
 
     def move(self):
-        self.acc = main.vec(0,0) 
+        self.acc = vec(0,0) 
         pressed_keys = pygame.key.get_pressed()            
         if pressed_keys[K_LEFT]:
-            self.acc.x = -(main.ACC)
+            self.acc.x = -(ACC)
         if pressed_keys[K_RIGHT]:
-            self.acc.x = main.ACC  
+            self.acc.x = ACC  
 
-        self.acc.x += self.vel.x * main.FRIC
+        self.acc.x += self.vel.x * FRIC
         self.vel += self.acc
         self.pos += self.vel + 0.5 * self.acc
 
-        if self.pos.x > main.WIDTH:
+        if self.pos.x > SCREEN_WIDTH:
             self.pos.x = 0
         if self.pos.x < 0:
-            self.pos.x = main.WIDTH
+            self.pos.x = SCREEN_WIDTH
      
         self.rect.midbottom = self.pos
